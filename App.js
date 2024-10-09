1408367ef4be9dd8be19ab388d6ee4ddb8d016dd
Eugene Chumak <evgeniy.ch.0@mail.ru>
2024-10-09 23:12:28 +0300

    Update App.js

--- App.js
+++ App.js
@@ -47,6 +47,34 @@ const AuthScreen = () => {
   );
 };
 
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    padding: 20,
+  },
+  title: {
+    fontSize: 24,
+    marginBottom: 20,
+    textAlign: 'center',
+  },
+  input: {
+    height: 40,
+    borderColor: 'gray',
+    borderWidth: 1,
+    marginBottom: 15,
+    paddingHorizontal: 10,
+  },
+  link: {
+    color: 'blue',
+    textAlign: 'center',
+    marginTop: 10,
+  },
+});
+
+export default AuthScreen;
+
+
 
 
 export default function App() {
